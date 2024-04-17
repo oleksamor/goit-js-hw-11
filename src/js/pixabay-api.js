526c1c6fc5d16206f8c6519fd6bef4a990a682b7
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-04-17 16:59:31 +0300

    upd render pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,2 +1,22 @@
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '43233775-39d154ba994f40051e2e1ec64';
 
+export function objectSearch(searchImage) {
+  const params = new URLSearchParams({
+    key: KEY,
+    q: searchImage,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${BASE_URL}/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+    return response.json();
+  });
+}
 
